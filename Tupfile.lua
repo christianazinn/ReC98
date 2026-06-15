24e92284bff0ef2ca48af48a5f62b8fd7a6962e6
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-14 22:38:18 -0400

    Decompile TH03 Mima boss render helper

--- Tupfile.lua
+++ Tupfile.lua
@@ -686,6 +686,7 @@ obj = {
 	"th03/main/main_03_dispatch.asm",
 	"th03/main_03p.cpp",
 	"th03/main/main_03_mima_update.asm",
+	"th03/main_03q.cpp",
 	{ "th03_main.asm", extra_inputs = {
 		th03_sprites["pellet"],
 		th03_sprites["score"],
