a9f6dc25ccc01556c1dc6c5c676beddb38e4d414
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-15 03:37:53 -0400

    Decompile TH03 Chiyuri record initializer

--- th03/main_03u.cpp
+++ th03/main_03u.cpp
@@ -22,6 +22,7 @@ extern "C" uint8_t byte_1F34E;
 extern "C" uint8_t byte_1F34F;
 extern "C" uint8_t byte_1F353;
 extern "C" uint8_t byte_1F355;
+extern "C" uint8_t byte_1F35E[];
 extern "C" uint8_t byte_1F3A0;
 
 extern "C" void pascal near sub_F58C(void);
@@ -113,4 +114,26 @@ extern "C" void pascal far gba_boss_render_kotohime(void)
 		return;
 	}
 	sub_F58C();
+}
+
+extern "C" void pascal far chiyuri_1219D(uint16_t slot)
+{
+	register uint8_t near *record = &byte_1F35E[slot * 32];
+
+	*reinterpret_cast<subpixel_t near *>(record + 0x00) = TO_SP(144);
+	*reinterpret_cast<subpixel_t near *>(record + 0x02) = TO_SP(80);
+	*reinterpret_cast<uint16_t near *>(record + 0x08) = 0xFFE0;
+	*reinterpret_cast<uint16_t near *>(record + 0x0A) = 0;
+	record[0x12] = 0;
+	record[0x10] = 0;
+	record[0x11] = 0;
+	*reinterpret_cast<uint16_t near *>(record + 0x0C) = 0x008C;
+	record[0x13] = 0;
+	record[0x17] = 0;
+	*reinterpret_cast<uint16_t near *>(record + 0x18) = 0;
+	*reinterpret_cast<uint16_t near *>(record + 0x0E) = 0x0288;
+	if(slot != 0) {
+		*reinterpret_cast<uint16_t near *>(record + 0x0E) += 0x28;
+	}
+	record[0x15] = 0;
 }
--- th03_main.asm
+++ th03_main.asm
@@ -6688,40 +6688,7 @@ KOTOHIME_11D1A procdesc near
 
 ; Attributes: bp-based frame
 
-chiyuri_1219D	proc far
-
-arg_0		= word ptr  6
-
-		push	bp
-		mov	bp, sp
-		push	si
-		mov	dx, [bp+arg_0]
-		mov	ax, dx
-		shl	ax, 5
-		add	ax, offset byte_1F35E
-		mov	si, ax
-		mov	word ptr [si], (144 shl 4)
-		mov	word ptr [si+2], (80 shl 4)
-		mov	word ptr [si+8], 0FFE0h
-		mov	word ptr [si+0Ah], 0
-		mov	byte ptr [si+12h], 0
-		mov	byte ptr [si+10h], 0
-		mov	byte ptr [si+11h], 0
-		mov	word ptr [si+0Ch], 8Ch
-		mov	byte ptr [si+13h], 0
-		mov	byte ptr [si+17h], 0
-		mov	word ptr [si+18h], 0
-		mov	word ptr [si+0Eh], 288h
-		or	dx, dx
-		jz	short loc_121EC
-		add	word ptr [si+0Eh], 28h ; '('
-
-loc_121EC:
-		mov	byte ptr [si+15h], 0
-		pop	si
-		pop	bp
-		retf	2
-chiyuri_1219D	endp
+	CHIYURI_1219D procdesc far
 
 
 ; =============== S U B	R O U T	I N E =======================================
