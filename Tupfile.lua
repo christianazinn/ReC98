2ee0d90c3a3a778d2b9d417a41b041737c72eee2
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-15 02:04:10 -0400

    Split TH03 Ellen update dispatcher

--- Tupfile.lua
+++ Tupfile.lua
@@ -691,6 +691,7 @@ obj = {
 	"th03/main_03r.cpp",
 	"th03/main/main_03_reimu_update.asm",
 	"th03/main_03s.cpp",
+	"th03/main/main_03_ellen_update.asm",
 	{ "th03_main.asm", extra_inputs = {
 		th03_sprites["pellet"],
 		th03_sprites["score"],
