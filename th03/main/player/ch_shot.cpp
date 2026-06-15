f500f12aee448f51a016abf5e06eb6865ec11147
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-15 08:00:48 -0400

    Decompile TH03 Marisa gauge pattern

--- th03/main/player/ch_shot.cpp
+++ th03/main/player/ch_shot.cpp
@@ -3,15 +3,25 @@
 #include "libs/master.lib/pc98_gfx.hpp"
 #include "platform.h"
 #include "th02/snd/snd.h"
+#include "th03/main/bullet/bullet.hpp"
 #include "th03/main/hitbox.hpp"
 #include "th03/main/hitcirc.hpp"
 #include "th03/main/player/cur.hpp"
+#include "th03/main/player/gba.hpp"
 #include "th03/main/player/stuff.hpp"
+#include "th03/main/playfld.hpp"
 #include "th03/main/v_colors.hpp"
+#include "th03/math/randring.hpp"
 
 extern "C" uint8_t byte_1FDEA;
 extern "C" uint8_t byte_1FE1C;
+extern "C" subpixel_t word_1FDE4[];
+extern "C" uint8_t byte_1FDE8[];
 extern "C" uint8_t near *word_1FE4E;
+extern "C" uint8_t byte_202B8[];
+
+extern "C" void pascal far sub_A3A8(uint8_t pid);
+extern "C" void pascal far SUB_CE0C(subpixel_t x, subpixel_t y, uint16_t pid);
 
 extern "C" void far sub_142D0(void)
 {
@@ -210,3 +220,91 @@ extern "C" void pascal far chargeshot_render_marisa(void)
 	grcg_off();
 	egc_on();
 }
+
+#define bullets_add_nopcall() { \
+	_asm { nop; push cs; call near ptr bullets_add; } \
+}
+
+void pascal near gauge_pattern_marisa(uint8_t type)
+{
+	uint8_t pid_other;
+	uint8_t flag_expected;
+	register int i;
+
+	flag_expected = GBAF_GAUGE_PELLET_INIT;
+	if(type == BT_BULLET16_DEFAULT) {
+		_AL = flag_expected;
+		_AL += GBAF_PELLET_TO_BULLET;
+		flag_expected = _AL;
+	}
+
+	if(gba_flag_active[pid_current] == flag_expected) {
+		word_1FDE4[pid_current] = (-randring2_next16_and(0x1F) << 4);
+		byte_1FDE8[pid_current] = 0;
+		gba_flag_active[pid_current]++;
+		byte_202B8[pid_current * 4] = (0x30 - gba_gauge_level[pid_current]);
+		return;
+	}
+
+	if(gba_flag_active[pid_current] != (flag_expected + 1)) {
+		return;
+	}
+
+	bullet_template.type = static_cast<bullet_type_t>(type);
+	bullet_template.center.y.v = word_1FDE4[pid_current];
+	pid_other = (1 - pid_current);
+	bullet_template.pid = pid_other;
+	bullet_template.speed.v = ((3 << 4) + 8);
+	bullet_template.group = BG_1;
+	if((byte_1FDE8[pid_current] % 0x18) == 8) {
+		SUB_CE0C(0, word_1FDE4[pid_current], static_cast<uint16_t>(pid_other));
+	} else if((byte_1FDE8[pid_current] % 0x18) == 0x14) {
+		SUB_CE0C(
+			(PLAYFIELD_W << 4),
+			word_1FDE4[pid_current],
+			static_cast<uint16_t>(pid_other)
+		);
+	} else if((byte_1FDE8[pid_current] % 0x18) == 0) {
+		bullet_template.angle = 0x20;
+		bullet_template.center.x.v = 0;
+		i = 0;
+		while(i < 5) {
+			bullets_add_nopcall();
+			bullet_template.speed.v += -6;
+			i++;
+		}
+		word_1FDE4[pid_current] += (byte_202B8[pid_current * 4] << 4);
+	} else if((byte_1FDE8[pid_current] % 0x18) == 0x0C) {
+		bullet_template.angle = 0x60;
+		bullet_template.center.x.v = (PLAYFIELD_W << 4);
+		i = 0;
+		while(i < 5) {
+			bullets_add_nopcall();
+			bullet_template.speed.v += -6;
+			i++;
+		}
+		word_1FDE4[pid_current] += (byte_202B8[pid_current * 4] << 4);
+	}
+
+	if(word_1FDE4[pid_current] >= (PLAYFIELD_H << 4)) {
+		gba_flag_active[pid_current] = GBAF_NONE;
+		sub_A3A8(pid_other);
+	}
+	byte_1FDE8[pid_current]++;
+}
+
+extern "C" void pascal far gba_gauge_pattern_pellet_marisa(void)
+{
+	if(gba_flag_active[pid_current] != GBAF_NONE) {
+		gauge_pattern_marisa(BT_PELLET);
+	}
+}
+
+extern "C" void pascal far gba_gauge_pattern_bullet_marisa(void)
+{
+	if(gba_flag_active[pid_current] != GBAF_NONE) {
+		gauge_pattern_marisa(BT_BULLET16_DEFAULT);
+	}
+}
+
+#undef bullets_add_nopcall
