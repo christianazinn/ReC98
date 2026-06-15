c91acbe18aa52e3d3a606acc2821ef7bacac4228
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-15 03:23:26 -0400

    Decompile TH03 Kotohime main render helper

--- th03/main_03u.cpp
+++ th03/main_03u.cpp
@@ -14,9 +14,12 @@ extern "C" subpixel_t word_1F33E;
 extern "C" subpixel_t word_1F340;
 extern "C" subpixel_t word_1F356;
 extern "C" uint16_t word_1F3B0;
+extern "C" sprite16_offset_t sprite_1F34C;
 extern "C" uint8_t pid_PID_so_attack;
+extern "C" uint8_t byte_1F34E;
 extern "C" uint8_t byte_1F353;
 extern "C" uint8_t byte_1F355;
+extern "C" uint8_t byte_1F3A0;
 
 extern "C" void pascal near kotohime_11FE4(int count)
 {
@@ -44,5 +47,25 @@ extern "C" void pascal near kotohime_11FE4(int count)
 		left = (playfield_fg_x_to_screen(left, (1 - pid_current)) - 16);
 		top = (top >> 4);
 		sprite16_put(left, top, sprite_offset);
+	}
+}
+
+extern "C" void pascal near kotohime_120A0(void)
+{
+	screen_x_t left;
+	screen_y_t top;
+	register sprite16_offset_t sprite_offset;
+
+	sprite16_put_size.w.v = (128 / 16);
+	sprite16_put_size.h = 48;
+	left = (playfield_fg_x_to_screen(word_1F33E, (1 - pid_current)) - 64);
+	top = ((word_1F340 >> 4) - 32);
+	sprite_offset = sprite_1F34C;
+	if(byte_1F34E != 0) {
+		sprite_offset += 0x10;
+	}
+	sprite16_put(left, top, sprite_offset);
+	if(byte_1F353 != 0) {
+		kotohime_11FE4(byte_1F3A0);
 	}
 }
