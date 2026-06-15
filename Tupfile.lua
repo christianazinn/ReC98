a5da26641808fcbf0cf6d377ffe5d3a33bedd833
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-15 00:10:42 -0400

    Decompile TH03 Yumemi render effect

--- Tupfile.lua
+++ Tupfile.lua
@@ -637,6 +637,7 @@ th03:branch(MODEL_LARGE, { cflags = "-DBINARY='M'" }):link("main", {
 	"th03/main/main_03_mima_update.asm",
 	"th03/main_03q.cpp",
 	"th03/main/main_03_yumemi_update.asm",
+	"th03/main_03r.cpp",
 	{ "th03_main.asm", extra_inputs = {
 		th03_sprites["pellet"],
 		th03_sprites["score"],
