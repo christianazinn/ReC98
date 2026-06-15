524d36700e6bf5f790609fef0652c356acb12142
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-14 21:39:48 -0400

    Decompile TH03 Marisa boss renderer

--- th03/main_03p.cpp
+++ th03/main_03p.cpp
@@ -5,6 +5,7 @@
 #include "platform.h"
 #include "th01/math/subpixel.hpp"
 #include "th03/main/player/cur.hpp"
+#include "th03/main/player/gba.hpp"
 #include "th03/main/playfld.hpp"
 #include "th03/main/round.hpp"
 #include "th03/main/sprite16.hpp"
@@ -12,9 +13,13 @@
 
 extern "C" subpixel_t word_1F33E;
 extern "C" subpixel_t word_1F340;
+extern "C" uint16_t word_1F3B0;
 extern "C" sprite16_offset_t sprite_1F34C;
 extern "C" uint8_t byte_1F34E;
+extern "C" uint8_t byte_1F34F;
 extern "C" uint8_t byte_1F353;
+
+extern "C" void pascal near sub_F58C(void);
 
 extern "C" void pascal near marisa_F9A6(void)
 {
@@ -93,4 +98,32 @@ extern "C" void pascal near marisa_FA71(screen_x_t distance)
 		mov dx, 0FFFFh
 		int SPRITE16
 	}
+}
+
+extern "C" void pascal far gba_boss_render_marisa(void)
+{
+	pid_t pid_other;
+
+	if(pid_current != gba_boss_launched_by) {
+		return;
+	}
+
+	pid_other = (1 - pid_current);
+	if(pid_other == 0) {
+		sprite16_clip.left = PLAYFIELD1_CLIP_LEFT;
+		sprite16_clip.right = PLAYFIELD1_CLIP_RIGHT;
+	} else {
+		sprite16_clip.left = PLAYFIELD2_CLIP_LEFT;
+		sprite16_clip.right = PLAYFIELD2_CLIP_RIGHT;
+	}
+
+	if(byte_1F34F == 0) {
+		marisa_FA71(200 - (word_1F3B0 * 2));
+		return;
+	}
+	if(byte_1F34F != 0xFF) {
+		marisa_F9A6();
+		return;
+	}
+	sub_F58C();
 }
