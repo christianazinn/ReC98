34af35c666c6da22c7f9cc3ccf5914c211991f60
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-15 07:33:59 -0400

    Decompile TH03 Marisa charge update

--- th03/main/player/ch_shot.cpp
+++ th03/main/player/ch_shot.cpp
@@ -58,3 +58,40 @@ extern "C" void pascal far marisa_hyper_14340(void)
 	word_1FE4E[1] = 0x10;
 	players[pid.current].shot_active = SA_DISABLED;
 }
+
+extern "C" void pascal far chargeshot_update_marisa(void)
+{
+	register int i;
+
+	word_1FE4E = (&byte_1FDEA + (pid_current * 0x32));
+	if(word_1FE4E[0] != 0) {
+		players[pid_current].gauge_charged = 0;
+		word_1FE4E[1]++;
+
+		i = 11;
+		while(i > 0) {
+			reinterpret_cast<Subpixel near *>(word_1FE4E + 2)[i] = (
+				reinterpret_cast<Subpixel near *>(word_1FE4E + 2)[i - 1]
+			);
+			reinterpret_cast<Subpixel near *>(word_1FE4E + 0x1A)[i].v = (
+				reinterpret_cast<Subpixel near *>(word_1FE4E + 0x1A)[i - 1].v + 0x18
+			);
+			i--;
+		}
+
+		if(word_1FE4E[1] < 0x18) {
+			reinterpret_cast<Subpixel near *>(word_1FE4E + 2)[0] = (
+				players[pid_current].center.x
+			);
+			reinterpret_cast<Subpixel near *>(word_1FE4E + 0x1A)[0].v = (
+				players[pid_current].center.y.v - 0x100
+			);
+			return;
+		}
+
+		if(word_1FE4E[1] > 0x24) {
+			players[pid_current].shot_active = SA_ENABLED;
+			word_1FE4E[0] = 0;
+		}
+	}
+}
