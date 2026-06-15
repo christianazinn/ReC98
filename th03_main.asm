8ca7554fec030157cb4adc17a1d7ab41e3f3c35f
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-15 15:53:29 -0400

    Decompile TH03 trapezoid idle callback

--- th03_main.asm
+++ th03_main.asm
@@ -2291,14 +2291,7 @@ public @GameExecl$qnxc
 
 ; Attributes: bp-based frame
 
-public SUB_B4A3
-SUB_B4A3 label near
-sub_B4A3	proc near
-		push	bp
-		mov	bp, sp
-		pop	bp
-		retn
-sub_B4A3	endp
+	SUB_B4A3 procdesc near
 
 
 ; =============== S U B	R O U T	I N E =======================================
