53207d312b8ef54d346ecc987dbc9974080856c2
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-15 14:58:06 -0400

    Decompile TH03 Mima bomb callback

--- th03/main/bullet/bullet.cpp
+++ th03/main/bullet/bullet.cpp
@@ -1,6 +1,8 @@
 #pragma option -zPmain_04 -G
 
 #include "libs/master.lib/master.hpp"
+#include "th03/formats/mrs.hpp"
+#include "th03/hardware/palette.hpp"
 #include "th03/main/bullet/bullet.hpp"
 #include "th03/main/player/bomb.hpp"
 #include "th03/main/player/cur.hpp"
@@ -10,6 +12,7 @@
 #include "th03/main/difficul.hpp"
 #include "th03/main/hitcirc.hpp"
 #include "th03/main/hitbox.hpp"
+#include "th03/main/playfld.hpp"
 #include "th03/main/sprite16.hpp"
 #include "th03/math/polar.hpp"
 #include "th03/math/randring.hpp"
@@ -23,6 +26,7 @@
 #include "libs/master.lib/pc98_gfx.hpp"
 #include "libs/sprite16/sprite16.h"
 #include "platform/x86real/flags.hpp"
+#include "codegen.hpp"
 #include "decomp.hpp"
 #include <stddef.h>
 
@@ -137,6 +141,7 @@ uint8_t bullet_trail_ring_i;
 
 extern "C" uint8_t byte_202B8[];
 extern "C" uint8_t byte_202B9[];
+extern "C" uint8_t byte_20E92[];
 extern "C" uint8_t byte_220DE[];
 extern "C" uint8_t byte_220E0;
 extern "C" uint8_t byte_220E6;
@@ -146,7 +151,13 @@ extern "C" uint8_t pid_PID_current;
 extern "C" uint8_t pid_PID_so_attack;
 
 extern "C" void pascal far sub_A3A8(uint8_t pid);
+extern "C" void pascal far SUB_CDBD(
+	subpixel_t x, subpixel_t y, uint16_t pid
+);
 extern "C" void pascal far SUB_CE5B(subpixel_t x, subpixel_t y, uint16_t pid);
+extern "C" void pascal far GRCG_OFF(void);
+extern "C" void far sub_B39E(void);
+extern "C" void pascal near mima_bomb_1714F(void);
 
 #define bullets_add_nopcall() { \
 	_asm { nop; push cs; call near ptr bullets_add; } \
@@ -653,6 +664,86 @@ narrow:
 #pragma option -k.
 #pragma warn +rch
 #pragma codeseg
+
+#pragma option -G-
+extern "C" void pascal far mima_bomb(void)
+{
+	uint8_t frame;
+	uint8_t col;
+
+	if(bomb_flag[pid_current] == BF_INACTIVE) {
+		return;
+	}
+	egc_off();
+	frame = bomb_frame[pid_current];
+	if(frame <= 80) {
+		grcg_setcolor(GC_RMW, pid_current);
+		_BX = FP_OFF(byte_20E92);
+		if(pid_current != 0) {
+			_BX += 0x28;
+		}
+		sub_B39E();
+		GRCG_OFF();
+		_AL = frame;
+		_AH = 0;
+		imul_reg_to_reg(_AX, _AX, 3);
+		_DL = 0;
+		_DL -= _AL;
+		col = _DL;
+		Palettes[pid_current].c.r = _DL;
+		Palettes[pid_current].c.g = _DL;
+		Palettes[pid_current].c.b = _DL;
+		palette_changed = true;
+		egc_on();
+		mima_bomb_1714F();
+		goto egc_on_and_ret;
+	} else if(frame < 144) {
+		if((frame & 1) == 0) {
+			snd_se_play(16);
+			Palettes[pid_current].c.r = 100;
+		} else {
+			Palettes[pid_current].c.r = 0;
+		}
+		Palettes[pid_current].c.g = 0;
+		Palettes[pid_current].c.b = 0;
+		palette_changed = true;
+
+		col = (frame % 8);
+		if(col == 0) {
+			SUB_CDBD(TO_SP(144), TO_SP(184), pid_current);
+		}
+		if((col & 3) < 2) {
+			playfield_fg_shift_x[pid_current] = 4;
+		} else {
+			playfield_fg_shift_x[pid_current] = -4;
+		}
+		mrs_put_noalpha_8(
+			(
+				(pid_current * PLAYFIELD_W_BORDERED) +
+				playfield_fg_shift_x[pid_current] +
+				PLAYFIELD_LEFT
+			),
+			PLAYFIELD_TOP,
+			(pid_current + 2),
+			(_AX = pid_current)
+		);
+	} else {
+		playfield_fg_shift_x[pid_current] = 0;
+		_AL = frame;
+		_AL <<= 3;
+		_DL = 255;
+		_DL -= _AL;
+		col = _DL;
+		Palettes[pid_current].c.r = _DL;
+		Palettes[pid_current].c.g = _DL;
+		Palettes[pid_current].c.b = _DL;
+		palette_changed = true;
+	}
+
+egc_on_and_ret:
+	egc_on();
+}
+#pragma option -G
 
 void bullets_reset(void)
 {
