ad7d17f1118805ad2c7645b704fe1c0ad2ede488
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-15 18:07:14 -0400

    Decompile TH03 Marisa extra attack render wrapper

--- th03/main/player/exatt.cpp
+++ th03/main/player/exatt.cpp
@@ -15,6 +15,7 @@ extern "C" uint8_t pid_PID_so_attack;
 extern "C" uint16_t word_2028A;
 extern "C" uint16_t far randring_far_next16_raw(void);
 extern "C" void pascal far SUB_CDBD(void);
+extern "C" void near marisa_19B4F(void);
 extern "C" void near sub_1A1A7(void);
 extern "C" void pascal near sub_1A1ED(
 	subpixel_t x,
@@ -26,6 +27,32 @@ extern "C" void pascal near sub_1A1ED(
 );
 extern "C" void pascal near sub_1A32A(screen_x_t left, screen_y_t top, uint8_t frame);
 extern "C" void pascal near sub_1A377(screen_x_t left, screen_y_t top, uint8_t frame);
+
+void far exatt_render_marisa(void)
+{
+	register uint8_t near *slot;
+	register int i;
+
+	_AL = pid_current;
+	_AH = 0;
+	_AX <<= 9;
+	_AX += reinterpret_cast<uint16_t>(exatt_buffers);
+	slot = reinterpret_cast<uint8_t near *>(_AX);
+
+	i = 0;
+	goto loop_test;
+loop:
+	if(slot[0] != 0) {
+		word_2028A = reinterpret_cast<uint16_t>(slot);
+		marisa_19B4F();
+	}
+	i++;
+	slot += 0x20;
+loop_test:
+	if(i < 0x0E) {
+		goto loop;
+	}
+}
 
 extern "C" void pascal far exatt_add_kotohime(
 	subpixel_t center_x, subpixel_t center_y, pid_t pid
