1f77efeace47b7f8e9ae93767f807ca93d0766a2
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-14 21:51:56 -0400

    Decompile TH03 Mima boss opening phase

--- th03/main_03p.cpp
+++ th03/main_03p.cpp
@@ -4,12 +4,13 @@
 #include "libs/sprite16/sprite16.h"
 #include "platform.h"
 #include "th01/math/subpixel.hpp"
+#include "th02/snd/snd.h"
+#include "th03/main/bullet/bullet.hpp"
 #include "th03/main/player/cur.hpp"
 #include "th03/main/player/gba.hpp"
 #include "th03/main/playfld.hpp"
 #include "th03/main/round.hpp"
 #include "th03/main/sprite16.hpp"
-#include "th03/sprites/main_s16.hpp"
 
 extern "C" subpixel_t word_1F33E;
 extern "C" subpixel_t word_1F340;
@@ -19,6 +20,12 @@ extern "C" sprite16_offset_t sprite_1F34C;
 extern "C" uint8_t byte_1F34E;
 extern "C" uint8_t byte_1F34F;
 extern "C" uint8_t byte_1F353;
+extern "C" uint8_t byte_1F354;
+extern "C" uint8_t byte_1F39F;
+extern "C" uint8_t byte_1F3A3;
+extern "C" uint8_t byte_1F3A4;
+extern "C" uint8_t byte_20E28;
+extern "C" uint8_t byte_20E29;
 
 extern "C" void pascal near sub_F58C(void);
 
@@ -147,4 +154,50 @@ extern "C" void pascal far mima_FB46(uint16_t slot)
 		*reinterpret_cast<uint16_t near *>(record + 0x0E) += 0x28;
 	}
 	record[0x15] = 0;
+}
+
+extern "C" void pascal near mima_FB95(void)
+{
+	if(word_1F3B0 < 2) {
+		byte_1F353 = 2;
+		byte_1F354 = 0;
+		byte_20E28 = 0;
+		byte_20E29 = 192;
+		return;
+	}
+
+	if(word_1F3B0 >= 0x20) {
+		if((word_1F3B0 & 1) == 0) {
+			snd_se_play(3);
+			bullet_template.angle = byte_20E28;
+			bullet_template.group = BG_2_SPREAD_HORIZONTALLY_SYMMETRIC;
+			bullet_template.center.x.v = word_1F33E;
+			bullet_template.center.y.v = word_1F340;
+			bullet_template.speed.v = byte_1F39F;
+			bullet_template.type = BT_BULLET16_DEFAULT;
+			bullet_template.is_animated = false;
+			bullets_add();
+			bullet_template.is_animated = true;
+			byte_20E28 += 9;
+
+			if(static_cast<uint16_t>(byte_1F3A4) > word_1F3B0) {
+				if((word_1F3B0 % 6) == 0) {
+					bullet_template.type = BT_PELLET;
+					bullet_template.group = BG_4_SPREAD_MEDIUM;
+					bullet_template.speed.v = byte_1F3A3;
+					bullet_template.angle = byte_20E29;
+					bullets_add();
+					bullet_template.angle = (0x80 - byte_20E29);
+					bullets_add();
+				}
+			}
+
+			byte_20E29 += 3;
+		}
+		if(word_1F3B0 > 0x82) {
+			byte_1F353 = 0;
+			byte_1F34F = 1;
+			word_1F3B0 = 0;
+		}
+	}
 }
