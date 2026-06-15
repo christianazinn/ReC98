a5ab6447d4b94103302ada749699301733b082f9
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-15 05:35:38 -0400

    Split TH03 Kana update dispatcher

--- Tupfile.lua
+++ Tupfile.lua
@@ -697,6 +697,7 @@ obj = {
 	"th03/main_03u.cpp",
 	"th03/main/main_03_chiyuri_update.asm",
 	"th03/main_03v.cpp",
+	"th03/main/main_03_kana_update.asm",
 	{ "th03_main.asm", extra_inputs = {
 		th03_sprites["pellet"],
 		th03_sprites["score"],
