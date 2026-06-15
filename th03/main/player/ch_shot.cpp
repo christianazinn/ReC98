1300d2cf1d34ae414c602ae04291e5572728e495
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-15 07:47:14 -0400

    Decompile TH03 Marisa charge render

--- th03/main/player/ch_shot.cpp
+++ th03/main/player/ch_shot.cpp
@@ -1,11 +1,13 @@
 #pragma codeseg HITBOX_TEXT
 
+#include "libs/master.lib/pc98_gfx.hpp"
 #include "platform.h"
 #include "th02/snd/snd.h"
 #include "th03/main/hitbox.hpp"
 #include "th03/main/hitcirc.hpp"
 #include "th03/main/player/cur.hpp"
 #include "th03/main/player/stuff.hpp"
+#include "th03/main/v_colors.hpp"
 
 extern "C" uint8_t byte_1FDEA;
 extern "C" uint8_t byte_1FE1C;
@@ -127,4 +129,84 @@ uint8_t far chargeshot_hittest_marisa(void)
 		i += 4;
 	}
 	return ret;
+}
+
+extern "C" void pascal far chargeshot_render_marisa(void)
+{
+	screen_y_t top;
+	screen_x_t last_left;
+	register int i;
+	register screen_x_t left;
+
+	word_1FE4E = (&byte_1FDEA + (pid_current * 0x32));
+	if(word_1FE4E[0] == 0) {
+		return;
+	}
+
+	egc_off();
+	grcg_settile_1line(GC_RMW, 0xAA0055AAL);
+	last_left = 0;
+	i = 11;
+	while(i > 6) {
+		left = playfield_fg_x_to_screen(
+			reinterpret_cast<Subpixel near *>(word_1FE4E + 2)[i].v,
+			pid_current
+		);
+		top = (
+			(reinterpret_cast<Subpixel near *>(word_1FE4E + 0x1A)[i].v >> 5) + 8
+		);
+		if(last_left != left) {
+			grcg_boxfill((left - 2), 8, (left + 1), top);
+		}
+		last_left = left;
+		i--;
+	}
+
+	grcg_settile_1line(GC_RMW, 0xFF00AA55L);
+	last_left = 0;
+	i = 6;
+	while(i > 3) {
+		left = playfield_fg_x_to_screen(
+			reinterpret_cast<Subpixel near *>(word_1FE4E + 2)[i].v,
+			pid_current
+		);
+		top = (
+			(reinterpret_cast<Subpixel near *>(word_1FE4E + 0x1A)[i].v >> 5) + 8
+		);
+		if(last_left != left) {
+			grcg_boxfill((left - 2), 8, (left + 1), top);
+		}
+		last_left = left;
+		i--;
+	}
+
+	grcg_settile_1line(GC_RMW, 0xFF55FF55L);
+	last_left = 0;
+	i = 3;
+	while(i > 0) {
+		left = playfield_fg_x_to_screen(
+			reinterpret_cast<Subpixel near *>(word_1FE4E + 2)[i].v,
+			pid_current
+		);
+		top = (
+			(reinterpret_cast<Subpixel near *>(word_1FE4E + 0x1A)[i].v >> 5) + 8
+		);
+		if(last_left != left) {
+			grcg_boxfill((left - 2), 8, (left + 1), top);
+		}
+		last_left = left;
+		i--;
+	}
+
+	grcg_setcolor(GC_RMW, V_WHITE);
+	left = playfield_fg_x_to_screen(
+		reinterpret_cast<Subpixel near *>(word_1FE4E + 2)[0].v,
+		pid_current
+	);
+	top = (
+		(reinterpret_cast<Subpixel near *>(word_1FE4E + 0x1A)[0].v >> 5) + 8
+	);
+	grcg_boxfill((left - 2), 8, (left + 1), top);
+	grcg_off();
+	egc_on();
 }
