d4441e8aaf584661e4f7f75edb86eb3cb7849cf0
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-16 05:28:56 -0400

    Decompile TH03 Kana charge-shot update callback

--- th03/main_09.cpp
+++ th03/main_09.cpp
@@ -3,10 +3,19 @@
 #include "platform.h"
 #include "th01/math/subpixel.hpp"
 #include "th03/main/player/cur.hpp"
+#include "th03/math/vector.hpp"
 
 extern "C" uint8_t kana_chargeshot_frames[];
 extern "C" uint8_t kana_chargeshot_nodes[];
 extern "C" uint8_t kana_chargeshot_state[];
+
+struct player_stuff_t {
+	uint8_t unused_0[0x18];
+	uint16_t gauge_charged;
+	uint8_t unused_1[0x66];
+};
+
+extern player_stuff_t players[PLAYER_COUNT];
 
 extern "C" void far sub_1BC4D(void)
 {
@@ -57,4 +66,91 @@ group_loop_test:
 	node[0x34] = 0xB8;
 	node -= 0x36;
 	node[0x34] = 0x90;
+}
+
+extern "C" void pascal far chargeshot_update_kana(void)
+{
+	subpixel_t vector_x;
+	subpixel_t vector_y;
+	int point_i;
+	uint8_t state;
+	register uint8_t near *node;
+	register int group_i;
+
+	if(kana_chargeshot_state[pid_current] == 0) {
+		goto ret;
+	}
+	players[pid_current].gauge_charged = 0;
+	state = kana_chargeshot_state[pid_current];
+	node = (kana_chargeshot_nodes + (pid_current * (4 * 54)));
+	group_i = 0;
+	goto group_loop_test;
+
+group_loop:
+	vector2(
+		vector_x,
+		vector_y,
+		node[0x34],
+		node[0x35]
+	);
+	point_i = 0x0C;
+	goto shift_loop_test;
+
+shift_loop:
+	reinterpret_cast<subpixel_t near *>(node)[point_i] = (
+		reinterpret_cast<subpixel_t near *>(node)[point_i - 1]
+	);
+	reinterpret_cast<subpixel_t near *>(node + 0x1A)[point_i] = (
+		reinterpret_cast<subpixel_t near *>(node + 0x1A)[point_i - 1]
+	);
+	point_i--;
+
+shift_loop_test:
+	if(point_i > 0) {
+		goto shift_loop;
+	}
+	reinterpret_cast<subpixel_t near *>(node)[0] += vector_x;
+	reinterpret_cast<subpixel_t near *>(node + 0x1A)[0] += vector_y;
+	if(state == 1) {
+		node[0x35]--;
+	}
+	group_i++;
+	node += 0x36;
+
+group_loop_test:
+	if(group_i < 4) {
+		goto group_loop;
+	}
+	if(state != 1) {
+		goto clear_test;
+	}
+	node -= 0x36;
+	node[0x34] += -2;
+	node -= (3 * 54);
+	node[0x34] += 2;
+	if(kana_chargeshot_frames[pid_current] <= 0x28) {
+		goto clear_test;
+	}
+	kana_chargeshot_state[pid_current] = 2;
+	group_i = 0;
+	goto length_loop_test;
+
+length_loop:
+	node[0x35] = 0x80;
+	group_i++;
+	node += 0x36;
+
+length_loop_test:
+	if(group_i < 4) {
+		goto length_loop;
+	}
+
+clear_test:
+	node = (kana_chargeshot_nodes + (pid_current * (4 * 54)));
+	if(*reinterpret_cast<int near *>(node + 0x32) <= -0x100) {
+		kana_chargeshot_state[pid_current] = 0;
+	}
+	kana_chargeshot_frames[pid_current]++;
+
+ret:
 }
