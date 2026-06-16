65eaf9d330ea62831125234c36b8e9b2ef68f79a
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-16 05:14:39 -0400

    Decompile TH03 Kana charge-shot state reset

--- Tupfile.lua
+++ Tupfile.lua
@@ -707,6 +707,7 @@ obj = {
 	"th03/main_06.cpp",
 	"th03/main_07.cpp",
 	"th03/main_08.cpp",
+	"th03/main_09.cpp",
 	{ "th03_main.asm", extra_inputs = {
 		th03_sprites["pellet"],
 		th03_sprites["score"],
