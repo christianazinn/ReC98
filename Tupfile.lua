f7d2f452b0343c782a49e732f75682f6ff5dafd4
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-16 20:13:40 -0400

    Decompile TH03 result overlay helper C2F9

--- Tupfile.lua
+++ Tupfile.lua
@@ -671,6 +671,7 @@ th03:branch(MODEL_LARGE, { cflags = "-DBINARY='M'" }):link("main", {
 	"th03/cfg_lres.cpp",
 	"th03/hitcirc.cpp",
 	"th03/hud_stat.cpp",
+	"th03/main/player/c2f9.cpp",
 	"th03/main/player/c433.cpp",
 	"th03/main/player/c54a.cpp",
 	"th03/main/player/c568.cpp",
