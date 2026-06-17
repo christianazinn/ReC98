7c7c7d11ec998a6143108c69309825c014c2c7bd
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-16 21:43:17 -0400

    Decompile TH03 HUD super strip blitter BDC2

--- Tupfile.lua
+++ Tupfile.lua
@@ -722,6 +722,7 @@ obj = {
 	"th03/cfg_lres.cpp",
 	"th03/hitcirc.cpp",
 	"th03/hud_stat.cpp",
+	"th03/main/player/bdc2.cpp",
 	"th03/main/player/be2a.cpp",
 	"th03/main/player/hudstart.cpp",
 	"th03/main/player/defeat.cpp",
