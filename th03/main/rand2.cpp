94fb01a7983d6d63f91884e90115b181b3b832e9
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-18 10:43:22 -0400

    Decompile TH03 far randring helpers

--- th03/main/rand2.cpp
+++ th03/main/rand2.cpp
@@ -36,3 +36,36 @@ uint16_t pascal near randring2_next16_mod(uint16_t divisor)
 	randring_p++;
 	return (*reinterpret_cast<uint16_t near *>(_BX) % divisor);
 }
+
+#pragma option -k-
+
+uint16_t far randring_far_next16(void)
+{
+	__emit__(0x32, 0xFF); // xor bh, bh
+	_BL = randring_p;
+	_asm { add	bx, offset randring; }
+	randring_p++;
+	return *reinterpret_cast<uint16_t near *>(_BX);
+}
+
+#pragma codestring "\x90"
+
+#pragma option -k.
+
+uint16_t pascal far randring_far_next16_and(uint16_t mask)
+{
+	__emit__(0x32, 0xFF); // xor bh, bh
+	_BL = randring_p;
+	_asm { add	bx, offset randring; }
+	randring_p++;
+	return (*reinterpret_cast<uint16_t near *>(_BX) & mask);
+}
+
+uint16_t pascal far randring_far_next16_mod(uint16_t divisor)
+{
+	__emit__(0x32, 0xFF); // xor bh, bh
+	_BL = randring_p;
+	_asm { add	bx, offset randring; }
+	randring_p++;
+	return (*reinterpret_cast<uint16_t near *>(_BX) % divisor);
+}
