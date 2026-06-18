1c511b6d4856a1476773365719d405892ae302a5
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-18 10:24:57 -0400

    Decompile TH03 randring next helpers

--- th03/main/hitc_mid.asm
+++ th03/main/hitc_mid.asm
@@ -2,15 +2,6 @@
 	.model use16 large _TEXT
 	locals
 
-include ReC98.inc
-include th03/th03.inc
-include th03/main/playfld.inc
-include th03/main/collmap.inc
-
-	extrn _randring:byte
-	extrn _randring_p:byte
-	extrn _collmap:byte:(PLAYER_COUNT * COLLMAP_SIZE)
-
 main_01 group PLAYFLD_TEXT, CFG_LRES_TEXT, HITCIRC_TEXT, HUD_STAT_TEXT, PLAYER_M_TEXT, main_010_TEXT, P_SHOT_TEXT
 
 PLAYFLD_TEXT segment byte public 'CODE' use16
@@ -34,8 +25,6 @@ P_SHOT_TEXT ends
 HITCIRC_TEXT segment word public 'CODE' use16
 		assume cs:main_01
 		assume es:nothing, ss:nothing, ds:_DATA, fs:nothing, gs:nothing
-
-RANDRING_NEXT_DEF_NOMOD 1, near
 
 HITCIRC_TEXT ends
 
--- th03/main/randfill.cpp
+++ th03/main/randfill.cpp
@@ -1,7 +1,11 @@
-#pragma option -zCHITCIRC_TEXT -zPmain_01 -k-
+#pragma option -zCHITCIRC_TEXT -zPmain_01
 
 #include "libs/master.lib/master.hpp"
 #include "th02/math/randring.hpp"
+
+extern uint8_t randring_p;
+
+#pragma option -k-
 
 void near randring_fill(void)
 {
@@ -11,4 +15,26 @@ void near randring_fill(void)
 	do {
 		randring[i] = irand();
 	} while(--i >= 0);
+}
+
+uint16_t near randring1_next16(void)
+{
+	__emit__(0x32, 0xFF); // xor bh, bh
+	_BL = randring_p;
+	_asm { add	bx, offset randring; }
+	randring_p++;
+	return *reinterpret_cast<uint16_t near *>(_BX);
+}
+
+#pragma codestring "\x90"
+
+#pragma option -k.
+
+uint16_t pascal near randring1_next16_and(uint16_t mask)
+{
+	__emit__(0x32, 0xFF); // xor bh, bh
+	_BL = randring_p;
+	_asm { add	bx, offset randring; }
+	randring_p++;
+	return (*reinterpret_cast<uint16_t near *>(_BX) & mask);
 }
