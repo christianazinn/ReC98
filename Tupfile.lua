88de01b48e044320591fdde30316f830050b2089
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-18 19:47:39 -0400

    Split MAINL verdict staffroll data suffix

--- Tupfile.lua
+++ Tupfile.lua
@@ -772,6 +772,7 @@ th03:branch(MODEL_LARGE, { cflags = "-DBINARY='L'" }):link("mainl", {
 	"th03/mainl/ccutdat.asm",
 	"th03/mainl/rsedat.asm",
 	{ "th03/mainl/fldsdat.asm", extra_inputs = th03_sprites["flake"] },
+	"th03/mainl/vstfdat.asm",
 	"th03/mainl/mainl_03_anchor.asm",
 	"th03/mainl/cdgunput.cpp",
 	"th03/mainl/stf_bclr.cpp",
