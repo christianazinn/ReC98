df84121400065c400ab890dda5611a9ffdd55a93
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-18 10:37:36 -0400

    Decompile TH03 randring2 helpers

---
+++ th03/main/rand2.cpp
@@ -0,0 +1,38 @@
+#pragma option -zCmain_04_TEXT -zPmain_04
+
+#include "th03/math/randring.hpp"
+
+extern uint8_t randring_p;
+
+#pragma option -k-
+
+uint16_t near randring2_next16(void)
+{
+	__emit__(0x32, 0xFF); // xor bh, bh
+	_BL = randring_p;
+	_asm { add	bx, offset randring; }
+	randring_p++;
+	return *reinterpret_cast<uint16_t near *>(_BX);
+}
+
+#pragma codestring "\x90"
+
+#pragma option -k.
+
+uint16_t pascal near randring2_next16_and(uint16_t mask)
+{
+	__emit__(0x32, 0xFF); // xor bh, bh
+	_BL = randring_p;
+	_asm { add	bx, offset randring; }
+	randring_p++;
+	return (*reinterpret_cast<uint16_t near *>(_BX) & mask);
+}
+
+uint16_t pascal near randring2_next16_mod(uint16_t divisor)
+{
+	__emit__(0x32, 0xFF); // xor bh, bh
+	_BL = randring_p;
+	_asm { add	bx, offset randring; }
+	randring_p++;
+	return (*reinterpret_cast<uint16_t near *>(_BX) % divisor);
+}
