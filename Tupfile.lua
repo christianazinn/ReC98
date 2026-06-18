35f530ac22b743fd30cce237f2e2c1508177cca1
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-18 17:42:39 -0400

    Split MAIN data player boss patterns prefix

--- Tupfile.lua
+++ Tupfile.lua
@@ -674,6 +674,7 @@ th03:branch(MODEL_LARGE, { cflags = "-DBINARY='M'" }):link("main", {
 	"th03/main/story_startup_data.asm",
 	"th03/main/support_format_data.asm",
 	"th03/main/gba_hitc_enemy_data.asm",
+	"th03/main/pbpat_data.asm",
 	"th03/main/round_gate_bss.asm",
 	"th03/main/support_resident_bss.asm",
 	"th03/main/palette_gba_bss.asm",
