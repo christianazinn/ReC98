ed83034b775d96a498e4ef5ff235f7fc4e8eb56c
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-18 00:11:10 -0400

    Decompile TH03 Ellen bomb setup callback

--- th03/main/player/chybomb.cpp
+++ th03/main/player/chybomb.cpp
@@ -9,12 +9,31 @@
 #include "th03/main/player/bomb.hpp"
 #include "th03/main/player/cur.hpp"
 #include "th03/main/playfld.hpp"
+#include "th03/math/vector.hpp"
 #include "x86real.h"
 
 extern "C" uint8_t byte_20E92[];
 
+struct ellen_bomb_vector_t {
+	subpixel_t x;
+	subpixel_t y;
+	subpixel_t velocity_x;
+	subpixel_t velocity_y;
+};
+
+static const int ELLEN_BOMB_VECTOR_COUNT = 8;
+static const subpixel_t ELLEN_BOMB_VECTOR_FREE = 9999;
+static const subpixel_t ELLEN_BOMB_VECTOR_INACTIVE = 19999;
+
+extern "C" ellen_bomb_vector_t ellen_bomb_vectors[
+	PLAYER_COUNT
+][ELLEN_BOMB_VECTOR_COUNT];
+extern "C" ellen_bomb_vector_t near *word_1FBBE;
+
 extern "C" void far sub_B39E(void);
 extern "C" void far SUB_A3D2(void);
+extern "C" void pascal far sub_A3A8(uint8_t pid);
+extern "C" uint16_t far randring_far_next16_raw(void);
 extern "C" void pascal far SUB_CE0C(
 	subpixel_t x, subpixel_t y, uint16_t pid
 );
@@ -108,4 +127,77 @@ extern "C" void pascal far chiyuri_bomb(void)
 	}
 
 	egc_on();
+}
+
+extern "C" void pascal far ellen_185AB(void)
+{
+	register int i;
+
+	if(bomb_flag[pid_current] == BF_INACTIVE) {
+		return;
+	}
+	if(bomb_flag[pid_current] == BF_PREPARING) {
+		bomb_flag[pid_current] = BF_ACTIVE;
+		bomb_frame[pid_current] = 0;
+		i = 0;
+		goto init_loop_test;
+
+init_loop:
+		ellen_bomb_vectors[pid_current][i].x = ELLEN_BOMB_VECTOR_INACTIVE;
+		i++;
+
+init_loop_test:
+		if(i < ELLEN_BOMB_VECTOR_COUNT) {
+			goto init_loop;
+		}
+		snd_se_play(17);
+	}
+
+	bomb_frame[pid_current]++;
+	playfield_clip_negative_radius.x.v = TO_SP(-32);
+	playfield_clip_negative_radius.y.v = TO_SP(-32);
+	word_1FBBE = &ellen_bomb_vectors[pid_current][0];
+
+	i = 0;
+	goto update_loop_test;
+
+update_loop:
+	if(word_1FBBE->x == ELLEN_BOMB_VECTOR_FREE) {
+		goto respawn;
+	}
+	if(word_1FBBE->x == ELLEN_BOMB_VECTOR_INACTIVE) {
+		goto next;
+	}
+	word_1FBBE->x += word_1FBBE->velocity_x;
+	word_1FBBE->y += word_1FBBE->velocity_y;
+	if(playfield_clip(
+		reinterpret_cast<PlayfieldSubpixel near *>(&word_1FBBE->x)[0],
+		reinterpret_cast<PlayfieldSubpixel near *>(&word_1FBBE->y)[0]
+	) == false) {
+		goto next;
+	}
+
+respawn:
+	word_1FBBE->x = TO_SP(144);
+	word_1FBBE->y = TO_SP(184);
+	vector2(
+		word_1FBBE->velocity_x,
+		word_1FBBE->velocity_y,
+		randring_far_next16_raw(),
+		224
+	);
+
+next:
+	i++;
+	word_1FBBE++;
+
+update_loop_test:
+	if(i < ELLEN_BOMB_VECTOR_COUNT) {
+		goto update_loop;
+	}
+
+	if(bomb_frame[pid_current] >= BOMB_FRAMES) {
+		bomb_flag[pid_current] = BF_INACTIVE;
+		sub_A3A8(pid_current);
+	}
 }
