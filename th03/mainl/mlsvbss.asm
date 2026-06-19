231da143f2e9edb72eed6c6ec636191d07a7a177
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-18 20:19:52 -0400

    Split MAINL staffroll verdict BSS tail

---
+++ th03/mainl/mlsvbss.asm
@@ -0,0 +1,13 @@
+	.386
+
+include libs/master.lib/macros.inc
+include th03/th03.inc
+
+DGROUP group _BSS
+
+_BSS segment word public 'BSS' use16
+	assume ds:DGROUP
+include th03/mainl/mlsv[bss].asm
+_BSS ends
+
+	end
