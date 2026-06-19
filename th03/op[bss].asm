3de89f86c242d1faf4a90a5e35efefdd325d3fbb
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-06-18 20:51:15 -0400

    Split OP support BSS

--- th03/op[bss].asm
+++ th03/op[bss].asm
@@ -13,5 +13,5 @@ public parfilename, pfint21_pf, pfint21_handle, pfint21_entries
 include libs/master.lib/pfint21[bss].asm
 include th03/hardware/input[bss].asm
 include th03/formats/cdg[bss].asm
+include th02/formats/pi_slots[bss].asm
 include th03/formats/hfliplut[bss].asm
-include th03/cutscene/cutscene[bss].asm
