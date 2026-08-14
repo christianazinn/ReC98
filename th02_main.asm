a1a949e50dbe49d941c047ad2565bc210ebba67c
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-08-14 16:45:56 -0700

    TH02: split main_01___TEXT at DemoPlay for a C++ contribution

--- th02_main.asm
+++ th02_main.asm
@@ -73,7 +73,7 @@ FACE_EXRIKA_SMILE = 153
 FACE_EXRIKA_FROWN = 156
 FACE_COL_0 = 255
 
-main_01 group main_01_TEXT, STAGE_INIT_TEXT, main_01__TEXT, STAGE_TEXT, main_01___TEXT, POINTNUM_TEXT, main_01____TEXT, ITEM_TEXT, HUD_TEXT, main_01_____TEXT, PLAYER_B_TEXT, PLAYER_TEXT, main_01______TEXT
+main_01 group main_01_TEXT, STAGE_INIT_TEXT, main_01__TEXT, STAGE_TEXT, main_01___TEXT, DEMO_TEXT, main_01____TEXT, POINTNUM_TEXT, main_01_____TEXT, ITEM_TEXT, HUD_TEXT, main_01______TEXT, PLAYER_B_TEXT, PLAYER_TEXT, main_01_______TEXT
 main_03 group main_03_TEXT, BULLET_TEXT, DIALOG_TEXT, BOSS_5_TEXT, main_03__TEXT
 main_06 group REGIST_M_TEXT, main_06_TEXT
 
@@ -1455,6 +1455,14 @@ loc_C1D0:
 		retf
 demo_load	endp
 
+main_01___TEXT	ends
+
+DEMO_TEXT	segment	byte public 'CODE' use16
+DEMO_TEXT	ends
+
+main_01____TEXT	segment	byte public 'CODE' use16
+		assume cs:main_01
+		assume es:nothing, ss:nothing, ds:_DATA, fs:nothing, gs:nothing
 
 ; =============== S U B	R O U T	I N E =======================================
 
@@ -1950,7 +1958,7 @@ var_2		= word ptr -2
 		leave
 		retn
 @player_invalidate$qv endp
-main_01___TEXT	ends
+main_01____TEXT	ends
 
 POINTNUM_TEXT	segment	byte public 'CODE' use16
 	@pointnums_init_for_rank_and_rese$qv procdesc near
@@ -1958,7 +1966,7 @@ POINTNUM_TEXT	segment	byte public 'CODE' use16
 	@pointnums_update_and_render$qv procdesc near
 POINTNUM_TEXT	ends
 
-main_01____TEXT	segment	word public 'CODE' use16
+main_01_____TEXT	segment	word public 'CODE' use16
 include th02/main/pointnum/num_put.asm
 
 ; =============== S U B	R O U T	I N E =======================================
@@ -3701,7 +3709,7 @@ loc_D61D:
 		leave
 		retn
 @shots_update_and_render$qv endp
-main_01____TEXT	ends
+main_01_____TEXT	ends
 
 ITEM_TEXT	segment	byte public 'CODE' use16
 	@items_init_and_reset$qv procdesc near
@@ -3726,7 +3734,7 @@ HUD_TEXT	segment	byte public 'CODE' use16
 	extern @overlay_stage_enter_animate$qv:proc
 HUD_TEXT	ends
 
-main_01_____TEXT	segment	byte public 'CODE' use16
+main_01______TEXT	segment	byte public 'CODE' use16
 
 ; =============== S U B	R O U T	I N E =======================================
 
@@ -4892,7 +4900,7 @@ loc_ECA9:
 		pop	bp
 		retn
 bomb_reimu_b	endp
-main_01_____TEXT	ends
+main_01______TEXT	ends
 
 PLAYER_B_TEXT	segment	byte public 'CODE' use16
 	@bomb_update_and_render$qv procdesc near
@@ -4912,7 +4920,7 @@ PLAYER_TEXT	segment	byte public 'CODE' use16
 	@player_update_and_render$qv procdesc near
 PLAYER_TEXT ends
 
-main_01______TEXT	segment	byte public 'CODE' use16
+main_01_______TEXT	segment	byte public 'CODE' use16
 
 ; =============== S U B	R O U T	I N E =======================================
 
@@ -5232,7 +5240,7 @@ off_F443	dw offset loc_F238
 		dw offset loc_F260
 		dw offset loc_F288
 		dw offset loc_F2A4
-main_01______TEXT	ends
+main_01_______TEXT	ends
 
 ; ===========================================================================
 
