29089d6c4397706003bb09d2a4eb4d4d19957b54
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-09-04 05:25:38 -0700

    Keep TH03 Selection helper DOS-compatible

--- Tupfile.lua
+++ Tupfile.lua
@@ -652,7 +652,7 @@ local th03_op_inputs = {
 	"th03/snd_midi.cpp",
 	"th03/lang_op.cpp",
 	"th03/scorio.cpp",
-	"th03/op/select_cdg_reload.cpp",
+	"th03/op/sldgrld.cpp",
 }
 th03_op_cfg:link("op", th03_inputs_replace(th03_op_inputs, {
 	["th03/language.cpp"] = th03_op_cfg:branch({
