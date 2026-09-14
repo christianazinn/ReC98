08f1ee2acbd2ff6918300c25663a3e9307bd0b71
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2026-09-13 19:42:56 -0700

    TH05 v0.1.4-rc5: Retain bomb background during hidden dialogue setup

--- th04/rp_dlg.cpp
+++ th04/rp_dlg.cpp
@@ -1,19 +1,23 @@
 // Included in DEMO_TEXT's patch reserve, in the native dialog's code group.
 #if (GAME == 4)
 void near dialog_init(void);
-#endif
 void near dialog_exit(void);
+#endif
+
+// TH05's hidden parser never draws faces. Keep its bomb background allocated:
+// boss sprites can otherwise fragment the freed block before dialog_exit()
+// tries to allocate the full background again, particularly without EMS.
 
 void far replay_practice_dialog_begin(void)
 {
 #if (GAME == 4)
 	dialog_init();
-#else
-	cdg_free(CDG_BG_PLAYCHAR_BOMB);
 #endif
 }
 
 void far replay_practice_dialog_end(void)
 {
+#if (GAME == 4)
 	dialog_exit();
+#endif
 }
